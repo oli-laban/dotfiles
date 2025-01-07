baacdf5a79fceb0ff939fc67c2145ec17a0b759c
Oli Laban <oli@olilaban.dev>
2025-01-07 22:37:35 +0000

    Configure nvim lsp + treesitter

--- .config/nvim/lua/olilaban/remap.lua
+++ .config/nvim/lua/olilaban/remap.lua
@@ -1,5 +1,33 @@
 local which_key = require("which-key")
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("user_lsp_attach", { clear = true }),
+  callback = function(event)
+    local opts = { buffer = event.buf }
+
+    local mappings = {
+      { "gd", vim.lsp.buf.definition, desc = "Go to definition" },
+      { "K",  vim.lsp.buf.hover,      desc = "Show hover information" },
+      {
+        icon = "LSP",
+        { "<leader>a", vim.lsp.buf.code_action,      desc = "Code action" },
+        { "<leader>r", vim.lsp.buf.references,       desc = "References" },
+        { "<leader>n", vim.lsp.buf.rename,           desc = "Rename" },
+        { "<leader>w", vim.lsp.buf.workspace_symbol, desc = "Workspace symbol" },
+      }
+    }
+
+    which_key.add(mappings, opts)
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = event.buf,
+      callback = function()
+        vim.lsp.buf.format({ async = false, id = event.data.client_id })
+      end
+    })
+  end
+})
+
 local general_mappings = {
   { "<leader>e",  vim.cmd.Ex,                                             desc = "Open file explorer" },
   { "<leader>p",  '"_dP',                                                 desc = "Paste without overwrite" },
