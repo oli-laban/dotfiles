fc3791f763c74823a68d94e2d33ce820695ffd64
Oli Laban <oli@olilaban.dev>
2025-01-08 10:39:25 +0000

    Add ayu theme, more nvim plugins

---
+++ .config/nvim/lua/olilaban/lazy/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  "Shatur/neovim-ayu",
+  config = function()
+    require("ayu").setup({
+      mirage = true,
+      terminal = true,
+    })
+    require("ayu").colorscheme()
+
+    local colors = require("ayu.colors")
+
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.comment })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.comment })
+  end
+}
